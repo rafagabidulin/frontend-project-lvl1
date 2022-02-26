0b9f8850c8d0b51b89bde680e2904509beec491b
rafagabidulin <rafagabidulin@gmail.com>
2022-02-26 16:10:31 +0300

    linting brain-games.js & cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,9 @@
 import readlineSync from 'readline-sync';
-
-console.log('Welcome to the Brain Games!');
-export const name = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${name}!`);
+export const print = () => {
+    console.log('Welcome to the Brain Games!');
+    const name = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${name}!`);
+}
 
 
 
